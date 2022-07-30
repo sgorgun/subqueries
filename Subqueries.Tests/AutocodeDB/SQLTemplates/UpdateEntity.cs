0964dfa6b97cc2a20fdaa67f0ba791409c56c08c
Yuriy Antonov <y.s.antonov.edu@gmail.com>
2022-07-30 21:30:10 +0300

    CI/CD: Update AutocodeDB Lib at 30/07/2022 21:25:34.32

--- Subqueries.Tests/AutocodeDB/SQLTemplates/UpdateEntity.cs
+++ Subqueries.Tests/AutocodeDB/SQLTemplates/UpdateEntity.cs
@@ -7,8 +7,10 @@ namespace AutocodeDB.SQLTemplates
         public static readonly string UpdateSet =
             $@"^\s*UPDATE\s*{TableEntity.TblNameIn}{TableEntity.TableName}{TableEntity.TblNameOut}\s*SET\s*((\s)|(\[))";
 
-        public static readonly string UpdateSetWhere = String.Format(@"{0}[\s\S]*?WHERE\s*{1}{2}{3}\s*{4}", UpdateSet,
-            TableEntity.ColNameIn, TableEntity.ColumnName, TableEntity.ColNameOut, OperationEntity.RelationalOperator);
+        public static readonly string UpdateSetWhere = String.Format($@"{UpdateSet}[\s\S]*?WHERE\s*{TableEntity.TableAndColumnName}\s*{OperationEntity.RelationalOperator}");
+
+/*        public static readonly string UpdateSetWhere = String.Format(@"{0}[\s\S]*?WHERE\s*{1}{2}{3}\s*{4}", UpdateSet,
+            TableEntity.ColNameIn, TableEntity.ColumnName, TableEntity.ColNameOut + "*", OperationEntity.RelationalOperator);*/
 
         public static readonly string UpdateSetWhereSubselect = String.Format(@"{0}\s*[(]\s*SELECT\s*{1}\s*FROM\s*{2}{3}{4}", UpdateSetWhere
             , $"(({FunctionEntity.Any})|({TableEntity.TableAndColumnName}))"
